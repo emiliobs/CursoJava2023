6db166b5f837f28d67aef400357330644122addd
barrera_eemilio@yahoo.es <>
2023-06-18 13:02:51 -0500

    Estructura for

---
+++ EstructuraFor/src/estructurafor/EstructuraFor.java
@@ -0,0 +1,21 @@
+
+package estructurafor;
+
+public class EstructuraFor 
+{  
+    public static void main(String[] args) 
+    {
+        System.out.println("Fin del Ciclo con for");
+        
+        for (int i = 0; i <= 10; i++) 
+        {
+            System.out.println("Estoy dando vuelta con FOR, estoy en la vuelta: " + i);        
+        
+        }
+   
+        System.out.println("Fin del Ciclo con for");
+    }
+    
+   
+    
+}
