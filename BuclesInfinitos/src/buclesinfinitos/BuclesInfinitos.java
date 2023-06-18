f2b41d001f6cab8479840cabdf280f17615bd36e
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-06-18 12:25:41 -0500

    Bucle Cintinela infinito

---
+++ BuclesInfinitos/src/buclesinfinitos/BuclesInfinitos.java
@@ -0,0 +1,22 @@
+
+package buclesinfinitos;
+
+
+public class BuclesInfinitos 
+{
+
+    
+    public static void main(String[] args)
+    {
+       //int contador = 0;
+        boolean cintinela = true;
+      
+        System.out.println("Bucle infinito.");
+        while (cintinela == true) 
+        {            
+            System.out.println("Estoy en la vuelta " + cintinela);
+            //contador += contador + 1;
+        }
+    }
+    
+}
