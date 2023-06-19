a35409f3c0d5d9dcd031963c90dfd68065a43395
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-06-19 17:14:40 -0500

    Add files via upload
    Caraga, Recorrdo,

--- VectoresDeclaracioAsignacion/src/vectoresdeclaracioasignacion/VectoresDeclaracioAsignacion.java
+++ VectoresDeclaracioAsignacion/src/vectoresdeclaracioasignacion/VectoresDeclaracioAsignacion.java
@@ -1,6 +1,8 @@
 
 package vectoresdeclaracioasignacion;
 
+import java.util.Scanner;
+
 public class VectoresDeclaracioAsignacion 
 {
 
@@ -14,19 +16,33 @@ public class VectoresDeclaracioAsignacion
         
                
         //Asignacion (Manual)
-        vector[0] = 2;
-        vector[1] = 77;
-        vector[2] = 177;
-        vector[3] = 998;
-        vector[4] = 555;
+//        vector[0] = 2;
+//        vector[1] = 77;
+//        vector[2] = 177;
+//        vector[3] = 998;
+//        vector[4] = 555;
+
+//ASignacion por teclado:
+        Scanner teclado = new Scanner(System.in);
+        
+        for (int i = 0; i < vector.length; i++) 
+        {
+            System.out.print("Ingrese el valor para el indice del Vector: ");
+           vector[i] = teclado.nextInt();
+        }
+        
+        System.out.println("");
+        System.out.println("Mostrando Valores del vector");
+        System.out.println("");
         
         //Recorrido:
         for (int i = 0; i < vector.length; i++) 
         {
             System.out.println("Posicion del vector "+ i + " con Valor: " + vector[i]);   
-            System.out.println("---------------------------------------------Declaracio, recorrido ");
+            System.out.println("---------------------------------------------");
         }
         
+         System.out.println("");
         System.out.println("Fin del lectura del Vector");
         
     }
