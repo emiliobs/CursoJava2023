e41e33406d568f05d9ee50a752594aa32a8db403
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-06-22 13:03:50 -0500

    INterface Grafica
    Limpiar y grabar con botones y mas...

---
+++ PruebaCapas/src/Logica/PruebaCapas.java
@@ -0,0 +1,17 @@
+
+package Logica;
+
+import Igu.Pantalla;
+
+public class PruebaCapas
+{
+   
+    public static void main(String[] args) 
+    {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+    
+}
